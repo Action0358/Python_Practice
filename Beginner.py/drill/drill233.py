a0c6f08bc7e380908073f9dd2709f14d1c8cc021
Action0358 <dai03588010@gmail.com>
2024-06-09 03:04:58 +0900

    drill232,233,234,235.pyを追加

---
+++ Beginner.py/drill/drill233.py
@@ -0,0 +1,3 @@
+# コード2-14  ディクショナリ要素の参照
+scores = {"network" :60, "database" :80, "security" :50}
+print(scores["database"]) # 要素を指定
---
+++ Beginner.py/drill/drill234.py
@@ -0,0 +1,5 @@
+# コード2-15 ディクショナリの要素の追加と変更
+scores = {"network" :60, "database" :80, "security" :55}
+scores["programming"] = 65
+scores["security"] = 55
+print(scores)
